19812cabdad00271d0bccdd6640b09a03578b4da
Rajendran R <rajendranr-5483@users.noreply.github.com>
2023-11-17 12:28:26 +0530

    Samples are added with default template files.

---
+++ Server with Per Page and Individual/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server_with_Per_Page_and_Individual.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
---
+++ ServerWASM with Per Page and Individual/ServerWASM with Per Page and Individual/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ServerWASM_with_Per_Page_and_Individual.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
