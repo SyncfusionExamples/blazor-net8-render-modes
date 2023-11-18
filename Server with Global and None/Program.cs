cef6eec3e73f8dc2135c9a9c4cb030099d278ac2
Rajendran R <rajendranr-5483@users.noreply.github.com>
2023-11-18 20:14:21 +0530

    Sample modified with Syncfusion Blazor components

--- Server with Global and None/Program.cs
+++ Server with Global and None/Program.cs
@@ -1,10 +1,12 @@
 using Server_with_Global_and_None.Components;
+using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
 
