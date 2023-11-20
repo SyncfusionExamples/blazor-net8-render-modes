32c108aebc65499d786e677b026cede9da9c53e3
Rajendran R <rajendranr-5483@users.noreply.github.com>
2023-11-20 17:26:10 +0530

    SSR rendering

--- SSR with Per Page and Individual/Program.cs
+++ SSR with Per Page and Individual/Program.cs
@@ -5,12 +5,13 @@ using Microsoft.EntityFrameworkCore;
 using SSR_with_Per_Page_and_Individual.Components;
 using SSR_with_Per_Page_and_Individual.Components.Account;
 using SSR_with_Per_Page_and_Individual.Data;
+using Syncfusion.Blazor;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
-
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
