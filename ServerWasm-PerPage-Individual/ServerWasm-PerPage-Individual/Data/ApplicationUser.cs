92d5ad6ec9bc1da75c1c35a74148a141b55f981c
Rajendran R <rajendranr-5483@users.noreply.github.com>
2023-11-20 19:13:42 +0530

    Sample modified

---
+++ ServerWasm-PerPage-Individual/ServerWasm-PerPage-Individual/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace ServerWasm_PerPage_Individual.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
