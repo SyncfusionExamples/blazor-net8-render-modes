ca098f056cefbf67d47c7994c6021ea3f9e30ee5
Rajendran R <rajendranr-5483@users.noreply.github.com>
2023-11-20 18:29:42 +0530

    Sample modified

--- ServerWASM with Global and Individual/ServerWASM with Global and Individual/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace ServerWASM_with_Global_and_Individual.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
